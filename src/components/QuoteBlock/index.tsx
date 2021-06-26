4c2bf45275e9dcd6b5197ea3c2a3d665a0e27941
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2021-06-26 18:39:48 +0900

    feat: Webフォント適用、ランダムボタン・クォートブロック作成(#3)

---
+++ src/components/QuoteBlock/index.tsx
@@ -0,0 +1,27 @@
+import { FC, ComponentPropsWithRef } from 'react';
+import { css } from '@emotion/react';
+
+type Props = ComponentPropsWithRef<'blockquote'>;
+
+const QuoteBlock: FC<Props> = ({ children, ...props }) => {
+  return (
+    <blockquote css={quoteBlock} {...props}>
+      <p css={quoteText}>{`“${children}”`}</p>
+    </blockquote>
+  );
+};
+
+const quoteBlock = css`
+  padding-left: 96px;
+  border-left: 8px solid #f7df94;
+`;
+
+const quoteText = css`
+  max-width: 616px;
+  font-family: Raleway, sans-serif;
+  font-size: 36px;
+  font-weight: 500;
+  line-height: 120%;
+`;
+
+export default QuoteBlock;
