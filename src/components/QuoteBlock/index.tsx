435cccdced780684d16548fbf55315a1988a760e
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2021-06-30 17:24:46 +0900

    feat: 特定の引用者の引用データ取得ページのスケルトン表示対応(#7)

--- src/components/QuoteBlock/index.tsx
+++ src/components/QuoteBlock/index.tsx
@@ -1,9 +1,22 @@
 import { FC, ComponentPropsWithRef } from 'react';
 import { css } from '@emotion/react';
+import Skeleton from '@/components/common/Skeleton';
 
-type Props = ComponentPropsWithRef<'blockquote'>;
+type Props = ComponentPropsWithRef<'blockquote'> & {
+  isLoading?: boolean;
+};
 
-const QuoteBlock: FC<Props> = ({ children, ...props }) => {
+const QuoteBlock: FC<Props> = ({ isLoading = false, children, ...props }) => {
+  if (isLoading) {
+    return (
+      <blockquote css={[quoteBlock, quoteSkeletonBox]} {...props}>
+        <Skeleton css={quoteSkeleton} width="100%" />
+        <Skeleton css={quoteSkeleton} width="100%" />
+        <Skeleton css={quoteSkeleton} width="100%" />
+      </blockquote>
+    );
+  }
+
   return (
     <blockquote css={quoteBlock} {...props}>
       <p css={quoteText}>{`“${children}”`}</p>
@@ -12,13 +25,34 @@ const QuoteBlock: FC<Props> = ({ children, ...props }) => {
 };
 
 const quoteBlock = css`
-  position: relative;
   padding-left: 96px;
   border-left: 8px solid #f7df94;
 
   @media (max-width: 600px) {
     padding-left: 40px;
     border-width: 4px;
+  }
+`;
+
+const quoteSkeletonBox = css`
+  display: grid;
+  grid-row-gap: 16px;
+
+  @media (max-width: 600px) {
+    grid-row-gap: 8px;
+  }
+`;
+
+const quoteSkeleton = css`
+  max-width: 616px;
+  height: 36px;
+
+  @media (max-width: 1280px) {
+    height: 32px;
+  }
+
+  @media (max-width: 600px) {
+    height: 20px;
   }
 `;
 
--- src/pages/[author].tsx
+++ src/pages/[author].tsx
@@ -1,16 +1,93 @@
-import { Fragment, useCallback } from 'react';
+import { VFC, Fragment, useCallback } from 'react';
 import { useRouter } from 'next/router';
 import { css } from '@emotion/react';
 import Layout from '@/components/Layout';
 import QuoteBlock from '@/components/QuoteBlock';
-import { useGetQuoteListInfiniteQuey } from '@/hooks/quote';
+import { useGetQuoteListInfiniteQuey } from '@/hooks//quote';
 import { useIntersectionObserver } from '@/hooks/util';
+import { QuoteData } from '@/models/Quote';
+
+type Props = {
+  isLoading: boolean;
+  quoteData: QuoteData[];
+  loadMoreRef: (node: Element) => void;
+  loadMoreMessage: string;
+};
+
+const QuoteBlockList: VFC<Props> = ({
+  isLoading,
+  quoteData,
+  loadMoreRef,
+  loadMoreMessage,
+}) => {
+  if (isLoading) {
+    return (
+      <div css={quoteBlockList}>
+        <QuoteBlock isLoading={isLoading} />
+        <QuoteBlock isLoading={isLoading} />
+        <QuoteBlock isLoading={isLoading} />
+      </div>
+    );
+  }
+
+  return (
+    <div css={quoteBlockList}>
+      {quoteData?.map((page) => (
+        <Fragment key={page.pagination.currentPage}>
+          {page.data.map((quote) => (
+            <QuoteBlock key={quote._id}>{quote.quoteText}</QuoteBlock>
+          ))}
+        </Fragment>
+      ))}
+      <div css={loadMoreBox} ref={loadMoreRef}>
+        {loadMoreMessage}
+      </div>
+    </div>
+  );
+};
+
+const quoteBlockList = css`
+  display: grid;
+  grid-template-columns: 616px;
+  grid-row-gap: 136px;
+
+  @media (max-width: 1280px) {
+    grid-row-gap: 96px;
+  }
+
+  @media (max-width: 600px) {
+    grid-template-columns: 1fr;
+    grid-row-gap: 64px;
+  }
+`;
+
+const authorNameText = css`
+  margin-left: 104px;
+  font-family: Raleway, sans-serif;
+  font-size: 36px;
+  font-weight: bold;
+  line-height: 42px;
+
+  @media (max-width: 600px) {
+    margin-left: 44px;
+    font-size: 32px;
+  }
+`;
+
+const loadMoreBox = css`
+  font-family: Raleway, sans-serif;
+  font-size: 18px;
+  font-weight: 500;
+  line-height: 120%;
+  text-align: center;
+`;
 
 const AuthorQuotes = () => {
   const router = useRouter();
   const authorName = (router.query.author as string)?.replace('_', ' ');
 
   const {
+    isLoading,
     data: quoteList,
     hasNextPage,
     isFetchingNextPage,
@@ -45,16 +122,12 @@ const AuthorQuotes = () => {
     <Layout onRandom={handleBackTopPage}>
       <main css={main}>
         <h2 css={authorNameText}>{authorName}</h2>
-        {quoteList?.pages?.map((page) => (
-          <Fragment key={page.pagination.currentPage}>
-            {page.data.map((quote) => (
-              <QuoteBlock key={quote._id}>{quote.quoteText}</QuoteBlock>
-            ))}
-          </Fragment>
-        ))}
-        <div css={loadMoreBox} ref={loadMoreRef}>
-          {loadMoreMessage}
-        </div>
+        <QuoteBlockList
+          isLoading={isLoading}
+          quoteData={quoteList?.pages}
+          loadMoreRef={loadMoreRef}
+          loadMoreMessage={loadMoreMessage}
+        />
       </main>
     </Layout>
   );
@@ -75,27 +148,6 @@ const main = css`
     grid-row-gap: 64px;
     padding: 80px 0;
   }
-`;
-
-const authorNameText = css`
-  margin-left: 104px;
-  font-family: Raleway, sans-serif;
-  font-size: 36px;
-  font-weight: bold;
-  line-height: 42px;
-
-  @media (max-width: 600px) {
-    margin-left: 44px;
-    font-size: 32px;
-  }
-`;
-
-const loadMoreBox = css`
-  font-family: Raleway, sans-serif;
-  font-size: 18px;
-  font-weight: 500;
-  line-height: 120%;
-  text-align: center;
 `;
 
 export default AuthorQuotes;
