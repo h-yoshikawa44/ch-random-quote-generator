7f7ec28ff2e9a98298f7281cde7ab251f1add5b8
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2023-05-30 19:29:30 +0900

    fix: QuoteGarden APIを新しいドメインに移行(#13)

--- src/config/ky.ts
+++ src/config/ky.ts
@@ -1,6 +1,6 @@
 import { Options } from 'ky';
 
 export const DEFAULT_API_OPTIONS: Options = {
-  prefixUrl: 'https://quote-garden.herokuapp.com/api/v3',
+  prefixUrl: 'https://quote-garden.onrender.com/api/v3',
   retry: 0,
 };
